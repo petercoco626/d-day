8e43977e3d889daab7b8ef626220e05aee21c178
petercoco626 <petercoco626@gmail.com>
2023-05-06 00:09:53 +0900

    [feat] 메타 태그 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import {
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import IWarningCircle from '@/assets/icon/WarningCircle.svg';
+import HeadMeta from '@/components/meta/headMeta';
 
 type InputForm = {
   startDate: Date;
@@ -58,78 +59,86 @@ export default function Home() {
   const inValid = () => {};
 
   return (
-    <main className="w-screen h-screen">
-      <div className="w-full h-[60px] bg-[#83C5BE] font-bold text-[22px] text-[#222222] flex items-center justify-center">
-        D-day 계산기, 날짜 계산기
-      </div>
-      <div className="w-full bg-[#EDF6F9] h-[calc(100%-60px)]">
-        <div className="w-[248px] h-full m-auto flex flex-col justify-between">
-          <div className="w-full flex flex-col gap-8 ">
-            <form
-              onSubmit={handleSubmit(valid, inValid)}
-              className="flex flex-col gap-4  mt-8 "
-            >
-              <div className="w-full gap-1 flex flex-col ">
-                <div className="w-full gap-1 flex justify-center items-center">
-                  <input
-                    type="date"
-                    {...register('startDate', {
-                      required: '날짜를 입력해 주세요',
-                    })}
-                    placeholder="날짜를 입력해 주세요."
-                    className="w-[200px] h-[48px] rounded-xl bg-white border-[1px] border-[#D8DFE1] border-solid px-[14px] py-[24px] box-border"
-                  />
-                  에서
-                </div>
-                {errors.startDate?.message && (
-                  <div className="text-[#E29578] font-normal text-xs ml-4 flex gap-1">
-                    <IWarningCircle />
-                    {errors.startDate?.message}
+    <>
+      <HeadMeta
+        title="d-day 날짜 계산기"
+        description="두개의 날짜를 선택하여 며칠이 남았는지 계산해보세요 !"
+        url="https://d-day-lyart.vercel.app"
+        keywords="d-day, 날짜 계산"
+      />
+      <main className="w-screen h-screen">
+        <div className="w-full h-[60px] bg-[#83C5BE] font-bold text-[22px] text-[#222222] flex items-center justify-center">
+          D-day 계산기, 날짜 계산기
+        </div>
+        <div className="w-full bg-[#EDF6F9] h-[calc(100%-60px)]">
+          <div className="w-[248px] h-full m-auto flex flex-col justify-between">
+            <div className="w-full flex flex-col gap-8 ">
+              <form
+                onSubmit={handleSubmit(valid, inValid)}
+                className="flex flex-col gap-4  mt-8 "
+              >
+                <div className="w-full gap-1 flex flex-col ">
+                  <div className="w-full gap-1 flex justify-center items-center">
+                    <input
+                      type="date"
+                      {...register('startDate', {
+                        required: '날짜를 입력해 주세요',
+                      })}
+                      placeholder="날짜를 입력해 주세요."
+                      className="w-[200px] h-[48px] rounded-xl bg-white border-[1px] border-[#D8DFE1] border-solid px-[14px] py-[24px] box-border"
+                    />
+                    에서
                   </div>
-                )}
-              </div>
-              <div className="w-full gap-1 flex flex-col ">
-                <div className="w-full gap-1 flex justify-center items-center">
-                  <input
-                    type="date"
-                    {...register('endDate', {
-                      required: '날짜를 입력해 주세요',
-                    })}
-                    placeholder="날짜를 입력해 주세요."
-                    className="w-[200px] h-[48px] rounded-xl bg-white border-[1px] border-[#D8DFE1] border-solid px-[14px] py-[24px] box-border"
-                  />
-                  까지
+                  {errors.startDate?.message && (
+                    <div className="text-[#E29578] font-normal text-xs ml-4 flex gap-1">
+                      <IWarningCircle />
+                      {errors.startDate?.message}
+                    </div>
+                  )}
                 </div>
-                {errors.endDate?.message && (
-                  <div className="text-[#E29578] font-normal text-xs ml-4 flex gap-1">
-                    <IWarningCircle />
-                    {errors.endDate?.message}
+                <div className="w-full gap-1 flex flex-col ">
+                  <div className="w-full gap-1 flex justify-center items-center">
+                    <input
+                      type="date"
+                      {...register('endDate', {
+                        required: '날짜를 입력해 주세요',
+                      })}
+                      placeholder="날짜를 입력해 주세요."
+                      className="w-[200px] h-[48px] rounded-xl bg-white border-[1px] border-[#D8DFE1] border-solid px-[14px] py-[24px] box-border"
+                    />
+                    까지
                   </div>
-                )}
+                  {errors.endDate?.message && (
+                    <div className="text-[#E29578] font-normal text-xs ml-4 flex gap-1">
+                      <IWarningCircle />
+                      {errors.endDate?.message}
+                    </div>
+                  )}
+                </div>
+                <button className="rounded-xl w-full h-[44px] flex items-center justify-center bg-[#006D77] text-base font-bold text-[#ffffff]">
+                  며칠 남았나요?
+                </button>
+              </form>
+              {result && (
+                <div className="flex flex-col gap-2 w-full h-[80px] rounded-xl bg-[#E29578] p-2 box-border justify-center items-center">
+                  <div>{result.resultD}</div>
+                  <div>{result.resultYMD}</div>
+                </div>
+              )}
+              <div className=" text-center font-normal text-sm text-[#666666] w-full h-[75px] p-2 box-border rounded-xl bg-[#D8DFE1] flex items-center justify-center">
+                기본으로 오늘 날짜로 설정되어 있습니다.
+                <br />
+                오늘을 포함해서 계산해 줍니다.
+                <br />
+                예시) 오늘~내일을 계산하면 D-1입니다.
               </div>
-              <button className="rounded-xl w-full h-[44px] flex items-center justify-center bg-[#006D77] text-base font-bold text-[#ffffff]">
-                며칠 남았나요?
-              </button>
-            </form>
-            {result && (
-              <div className="flex flex-col gap-2 w-full h-[80px] rounded-xl bg-[#E29578] p-2 box-border justify-center items-center">
-                <div>{result.resultD}</div>
-                <div>{result.resultYMD}</div>
-              </div>
-            )}
-            <div className=" text-center font-normal text-sm text-[#666666] w-full h-[75px] p-2 box-border rounded-xl bg-[#D8DFE1] flex items-center justify-center">
-              기본으로 오늘 날짜로 설정되어 있습니다.
-              <br />
-              오늘을 포함해서 계산해 줍니다.
-              <br />
-              예시) 오늘~내일을 계산하면 D-1입니다.
+            </div>
+            <div className="text-[#AAAAAA] font-normal text-xs w-full p-2 box-border text-center">
+              ⓒ 2023. zizzi project all rights reserved.
             </div>
           </div>
-          <div className="text-[#AAAAAA] font-normal text-xs w-full p-2 box-border text-center">
-            ⓒ 2023. zizzi project all rights reserved.
-          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
